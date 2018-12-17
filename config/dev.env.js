f30cfffb32263c17bdb8b23ca8e72e141a36e5f8
geekerstar <247507792@qq.com>
2018-12-17 20:07:39 +0800

    v0.1

---
+++ config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"https://www.easy-mock.com/mock/5c1329e37ab16c3d781eb769/verynavi"',
+})
---
+++ config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  BASE_API: '"https://www.easy-mock.com/mock/5c1329e37ab16c3d781eb769/verynavi"',
+}
